9cab7b6226b817972d77449a11247f512a400631
Taxa8888 <Taxa8888@bk.ru>
2020-10-18 00:37:51 +0300

    feat: resolve some task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let arr = [];
+    members.forEach(function (name) {
+      if (typeof name === 'string') {
+        name = name.trim();
+        arr.push(name[0].toUpperCase());
+      }
+    })
+    return arr.sort().join('');
+  } else {
+    return false;
+  }
 };
+
+
+
+// let members = [
+//   'amelia',
+//   'ruby',
+//   'lily',
+//   'grace',
+//   'millie',
+//   'daisy',
+//   'freya',
+//   'erin',
+//   'megan',
+//   'jasmine',
+//   'brooke',
+// ]
+// console.log(createDreamTeam(members))
